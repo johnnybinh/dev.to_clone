ee1970c4c0a536840f32f65c7cb02f98423a761a
johnnybinh <thebinhnguyen2703@gmail.com>
2024-04-04 20:54:56 +0700

    dev

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 //import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="bg-white w-full h-[56px] border-b-[1px] top-0 sticky">
       <div className="mx-10 w-full h-full flex ">
-        <img
-          src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
-          alt="devlogo"
-          className="w-[50px] h-[40px] my-auto"
-        />
+        <Link className="h-full flex" to="/">
+          <img
+            src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+            alt="devlogo"
+            className="w-[50px] h-[40px] my-auto"
+          />
+        </Link>
         <form className="h-full flex" action="">
           <input
             className="h-[40px] mx-2 w-[420px] outline-0 p-1 focus:border-[2px] focus:border-blue-800 my-auto border-[1px]  border-gray-300 rounded-[0.25rem]"
@@ -19,15 +21,15 @@ const NavBar = () => {
             id=""
           />
         </form>
-        <a className="my-auto ml-[25em] font-medium" href="">
+        <Link className="my-auto ml-[25em] font-medium" to="/login">
           <center>Login</center>
-        </a>
-        <a
+        </Link>
+        <Link
           className="my-auto flex ml-10 w-40 h-[40px] font-medium text-blue-500 justify-center items-center border-2 rounded-lg border-blue-500 hover:bg-blue-500"
-          href=""
+          to="/signup"
         >
           Create Account
-        </a>
+        </Link>
       </div>
     </div>
   );
